9ec9fe1ee082059b449d626ada8cd4a72fdaabb7
mogaji tomiyin <tomiyin360@gmail.com>
2023-10-22 12:52:29 +0100

    modified main.py

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():	
+    return render_template('Home page.html')
+
 @app.route('/login_signup')
 def login_signup():
     return render_template('/Login_Signup.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    pass
+
 
 
 if __name__ == '__main__':
