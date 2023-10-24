b3c3317f5626813fc428e01f12355595e1a0dd25
marsuvees <tolujed@gmail.com>
2023-10-24 06:30:46 +0100

    minor updates

--- auth.py
+++ auth.py
@@ -1,22 +1,51 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session, g
 from database import session as sess, Users
+from bcrypt import checkpw, gensalt, hashpw
 
+salt = gensalt()
 auth_bp = Blueprint('auth', __name__)
 
-@auth_bp.route('/login', methods=['GET', 'POST'])
-def login():
+# @auth_bp.route('/create_account', methods=['GET', 'POST'])
+# def create_account():
+#     if request.method == 'POST':
+#         name = request.form['Name']
+#         email = request.form['Em@il']
+#         password = request.form['Pass_word']
+#         user = sess.query(Users).filter_by(email=email).first()
+#         if user:
+#             flash('Email already exists')
+#         else:
+#             new_user = Users(name=name, email=email, password=hashpw(password.encode('utf-8'), salt).decode('utf-8'))
+#             sess.add(new_user)
+#             sess.commit()
+#             return redirect(url_for('auth.sign_in'))
+#     return render_template('Sign in.html')
+
+@auth_bp.route('/accounts', methods=['GET', 'POST'])
+def accounts():
     if request.method == 'POST':
-        email = request.form['E']
-        password = request.form['password']
+        name = request.form['Name']
+        email = request.form['Em@il']
+        password = request.form['Pass_word']
         user = sess.query(Users).filter_by(email=email).first()
-        if user and user.password == password:
-            session['user_id'] = user.id
+        if user:
+            flash('Email already exists')
+        else:
+            new_user = Users(name=name, email=email, password=hashpw(password.encode('utf-8'), salt).decode('utf-8'))
+            sess.add(new_user)
+            sess.commit()
+            return redirect(url_for('auth.acounts'))
+        login_email = request.form['Email']
+        login_password = request.form['Password']
+        existing_user = sess.query(Users).filter_by(email=login_email).first()
+        hashed_pass = user.password
+        if user and checkpw(login_password.encode('utf-8'), hashed_pass.encode('utf-8')):
+            session['user_id'] = existing_user.id
             return redirect(url_for('main.home'))
         else:
             flash('Invalid username or password')
-
     return render_template('Sign in.html')
-    pass 
+     
 
 if __name__ == '__main__':
     pass
